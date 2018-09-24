fc171807d6f1e6165cb7d621761cb6bde9a43612
Preyansh Mitharwal <jonysikar0007@gmail.com>
2018-09-24 10:53:13 +0530

    countries are selectable

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ import { Cylindrical } from "three";
 
 console.log(AFRAME.version);
 
+let totalFundsProcessed;
+let fundsProcessedBeforeCutoff;
+let fundsProcessedAfterCutoff;
+let totalWIP;
+let wipWellWithinCutoff;
+let wipAboutToMissCutoff;
+let totalMissed;
+
+let totalFundsProcessedValue;
+let fundsProcessedBeforeCutoffValue;
+let fundsProcessedAfterCutoffValue;
+let totalWIPValue;
+let wipWellWithinCutoffValue;
+let wipAboutToMissCutoffValue;
+let totalMissedValue;
+
 const totalFunds = 2480;
 const countryFunds = {
     france: 1200,
@@ -14,6 +30,17 @@ const countryFunds = {
     ireland: 10,
     india: 1800,
 };
+
+const countryNames = {
+    france: "France",
+    germany: "Germany",
+    italy: "Italy",
+    luxembourg: "Luxembourg",
+    india: "India",
+    ireland: "Ireland",
+    global: "Global",
+    uk: "United Kingdom"
+}
 
 const data = {
     france: {
@@ -147,7 +174,7 @@ const data = {
 }
 
 AFRAME.registerComponent("show-funds", {
-    schema: {default: ''},
+    schema: { default: '' },
     init() {
         console.log(this.data)
         this.el.addEventListener("click", () => {
@@ -189,35 +216,60 @@ AFRAME.registerComponent("zooming", {
 AFRAME.registerComponent("select-view", {
     schema: { default: "" },
     init() {
-       
-       this.el.addEventListener("click", () => {
-        console.log("working")
-        const globe = document.querySelector("#globe");
-        const pos = globe.getAttribute("position");
-        // globe.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z - 5});
-        // globe.setAttribute("material", "opacity: 0.5");
-        if (this.data == "global") {
-            globe.setAttribute("material", "opacity: 1");
-            globe.emit("goingforwards")
-            // document.querySelector("#ocean").setAttribute("visible", "false");
-        } else {
-            // document.querySelector("#ocean").setAttribute("visible", "true");
-            globe.setAttribute("material", "opacity: 0.5");
-            globe.emit('goingbackwards');
-        }
-        const countryFocusPlane = document.getElementById("countryFocusPlane");
-        countryFocusPlane.setAttribute("material", `src: #${this.data}Map; transparent: true`);
-        // countryFocusPlane.setAttribute("visible", "true");
 
-        const totalFundsProcessed = document.querySelector("#maincyl");
-        const fundsProcessedBeforeCutoff = document.querySelector("#maincyl-2");
-        const fundsProcessedAfterCutoff = document.querySelector("#maincyl-3");
-        const viewData = data[this.data];
-        console.log(this.data)
-        totalFundsProcessed.setAttribute("height", viewData["processed"].total / 1000);
-        fundsProcessedBeforeCutoff.setAttribute("height", viewData["processed"].withinCutoff / 1000);
-        fundsProcessedAfterCutoff.setAttribute("height", viewData["processed"].afterCutoff / 1000);
-       }) 
+        this.el.addEventListener("click", () => {
+            console.log("working")
+            const globe = document.querySelector("#globe");
+            const pos = globe.getAttribute("position");
+            // globe.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z - 5});
+            // globe.setAttribute("material", "opacity: 0.5");
+            if (this.data == "global") {
+                globe.setAttribute("material", "opacity: 1");
+                document.getElementById("heading").setAttribute("value", countryNames[this.data] + " View");
+                // globe.emit("goingforwards")
+                // document.querySelector("#ocean").setAttribute("visible", "false");
+                document.querySelector("#countryFocusPlane").setAttribute("visible", "false");
+            } else {
+                // document.querySelector("#ocean").setAttribute("visible", "true");
+                // globe.setAttribute("material", "opacity: 0.5");
+                globe.emit("country-map-appearing");
+                const countryFocusPlane = document.querySelector("#countryFocusPlane");
+                countryFocusPlane.setAttribute("material", `src: #${this.data}Map; transparent: true`);
+                countryFocusPlane.emit("country-map-appearing");
+                countryFocusPlane.setAttribute("visible", "true");
+                document.getElementById("heading").setAttribute("value", countryNames[this.data] + " View");
+                // globe.emit('goingbackwards');
+            }
+            const totalFundsProcessed = document.querySelector("#total-processed");
+            const fundsProcessedBeforeCutoff = document.querySelector("#processed-before-cutoff");
+            const fundsProcessedAfterCutoff = document.querySelector("#processed-after-cutoff");
+
+            const totalWIP = document.querySelector("#total-wip");
+            const wipWellWithinCutoff = document.querySelector("#wip-well-within-cutoff");
+            const wipAboutToMissCutoff = document.querySelector("#wip-about-to-miss-cutoff");
+
+            const totalMissed = document.querySelector("#total-missed");
+
+            
+
+
+            const viewData = data[this.data];
+            console.log(viewData)
+
+            const deno1 = 500;
+            const deno2 = 100;
+            const deno3 = 200;
+
+            totalFundsProcessed.setAttribute("height", viewData["processed"].total / deno1);
+            fundsProcessedBeforeCutoff.setAttribute("height", viewData["processed"].withinCutoff / deno1);
+            fundsProcessedAfterCutoff.setAttribute("height", viewData["processed"].afterCutoff / deno1);
+
+            totalWIP.setAttribute("height", viewData["wip"].total / deno3);
+            wipWellWithinCutoff.setAttribute("height", viewData["wip"].wellWithinTime / deno3);
+            wipAboutToMissCutoff.setAttribute("height", viewData["wip"].aboutToMissCutoff / deno3);
+
+            totalMissed.setAttribute("height", viewData["missedCutoff"].total / deno2);
+        })
     }
 })
 
@@ -232,6 +284,61 @@ AFRAME.registerComponent("swap-plane", {
     }
 })
 
+AFRAME.registerComponent("global-view", {
+    schema: { default: "" },
+    init() {
+        document.querySelector('a-scene').addEventListener('loaded', () => {
+            console.log('loaded')
+            this.totalFundsProcessed = document.querySelector("#total-processed");
+            this.fundsProcessedBeforeCutoff = document.querySelector("#processed-before-cutoff");
+            this.fundsProcessedAfterCutoff = document.querySelector("#processed-after-cutoff");
+        
+            this.totalWIP = document.querySelector("#total-wip");
+            this.wipWellWithinCutoff = document.querySelector("#wip-well-within-cutoff");
+            this.wipAboutToMissCutoff = document.querySelector("#wip-about-to-miss-cutoff");
+        
+            this.totalMissed = document.querySelector("#total-missed");
+        
+            const viewData = data["global"];
+            console.log(viewData)
+        
+            const deno1 = 500;
+            const deno2 = 100;
+            const deno3 = 200;
+        
+            this.totalFundsProcessed.setAttribute("height", viewData["processed"].total / deno1);
+            this.fundsProcessedBeforeCutoff.setAttribute("height", viewData["processed"].withinCutoff / deno1);
+            this.fundsProcessedAfterCutoff.setAttribute("height", viewData["processed"].afterCutoff / deno1);
+        
+            this.totalWIP.setAttribute("height", viewData["wip"].total / deno3);
+            this.wipWellWithinCutoff.setAttribute("height", viewData["wip"].wellWithinTime / deno3);
+            this.wipAboutToMissCutoff.setAttribute("height", viewData["wip"].aboutToMissCutoff / deno3);
+        
+            this.totalMissed.setAttribute("height", viewData["missedCutoff"].total / deno2);
+
+
+            this.totalFundsProcessedValue = document.querySelector("#total-processed-value");
+            this.fundsProcessedBeforeCutoffValue = document.querySelector("#processed-before-cutoff-value");
+            this.fundsProcessedAfterCutoffValue = document.querySelector("#processed-after-cutoff-value");
+
+            this.totalWIPValue = document.querySelector("#total-wip-value");
+            this.wipWellWithinCutoffValue = document.querySelector("#wip-well-within-cutoff-value");
+            this.wipAboutToMissCutoffValue = document.querySelector("#wip-about-to-miss-cutoff-value");
+
+            this.totalMissedValue = document.querySelector("#total-missed-value");
+
+            this.totalFundsProcessedValue.setAttribute("value", viewData["processed"].total);
+            this.fundsProcessedBeforeCutoffValue.setAttribute("value", viewData["processed"].withinCutoff);
+            this.fundsProcessedAfterCutoffValue.setAttribute("value", viewData["processed"].afterCutoff);
+
+            this.totalWIPValue.setAttribute("value", viewData["wip"].total);
+            this.wipWellWithinCutoffValue.setAttribute("value", viewData["wip"].wellWithinTime);
+            this.wipAboutToMissCutoffValue.setAttribute("value", viewData["wip"].aboutToMissCutoff);
+
+            this.totalMissedValue.setAttribute("value", viewData["missedCutoff"].total);
+        });
+    }
+})
 // var cameraEl = document.querySelector('#camera');
 // var worldPos = new THREE.Vector3();
 // worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
